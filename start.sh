5d25c3dc671dc6689796b93c9b84e2791c227b67
ComfyUIEnthusiast <comfyuidev@gmail.com>
2025-01-25 15:07:09 -0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+cd /workspace/ComfyUI/custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
 cd /workspace/ComfyUI
 cp -r -u /workspace/importModels/* /workspace/ComfyUI/models
 python -m jupyter notebook --NotebookApp.token=$JUPYTER_TOKEN --NotebookApp.ip='0.0.0.0' --port $JUPYTER_PORT --allow-root & 
