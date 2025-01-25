718a19fa0d29e75a8a7fc91e6106807510306470
ComfyUIEnthusiast <comfyuidev@gmail.com>
2025-01-25 13:47:37 -0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 cd /workspace/ComfyUI
-cp -r -u /workspace/importModels/* /workspace/models
+cp -r -u /workspace/importModels/* /workspace/ComfyUI/models
 python -m jupyter notebook --NotebookApp.token=$JUPYTER_TOKEN --NotebookApp.ip='0.0.0.0' --port $JUPYTER_PORT --allow-root & 
 python main.py --listen 0.0.0.0 --port $COMFYUI_PORT &
 wait
